e91f179dfab0b6ef133e7320935c51e1f500cdc5
HamzaSameen462 <hamzasameen2003@gmail.com>
2025-01-29 14:05:24 +0000

    Datatype

---
+++ JS_Basics/02_datatypes.js
@@ -0,0 +1,8 @@
+
+"use strict";// treat all js code as a newer version
+
+//alert(3+3) here its not posible beacuse we are uing node.js not browser
+
+alert(3
+    +3
+)// Code readeability should be high 
